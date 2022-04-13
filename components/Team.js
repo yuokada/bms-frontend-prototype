5715db780ef16e155d3e7dc6b5c351cb32de4a85
Yukihiro Okada <yuokada@users.noreply.github.com>
2022-04-14 00:14:36 +0900

    Introduce team API to index.js

---
+++ components/Team.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Team = ({team}) => {
+    return (
+        <div>
+            <Link href={`/teams/${team.url_path}`}>
+                <a className={"cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200"}>
+                    {team.name}
+                </a>
+            </Link>
+        </div>
+    )
+}
+
+export default Team;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Layout from "../components/Layout"
+import {getAllTeamsData} from "../lib/teams";
+import Team from "../components/Team";
 
-export default function Home() {
-  return (
-      <Layout>
-          <main className={""}>Welcome to BMS!</main>
-      </Layout>
-  )
+export default function Home({teams}) {
+    return (
+        <Layout title={"BMS - Baseball Management System"}>
+            <div className={""}>Welcome to BMS!</div>
+            <ui className="m-10">
+                {teams && teams.map((team) => <Team key={team.id} team={team}/>)
+                }
+            </ui>
+        </Layout>
+    )
+}
+
+
+export async function getStaticProps() {
+    // NOTE: この関数はビルド時に実行される.
+    const teams = await getAllTeamsData();
+    return {
+        props: {
+            teams
+        }
+    }
 }
