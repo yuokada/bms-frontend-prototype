80288eff566cf8597d414d8e703c9ba052764f61
Yukihiro Okada <yuokada@users.noreply.github.com>
2025-09-01 00:07:59 +0900

    Upgrade tailwindCSS to v4

--- components/Auth.js
+++ components/Auth.js
@@ -79,14 +79,14 @@ export default function Auth() {
                 </div>
                 <form className="mt-8 space-y-6" onSubmit={authUser}>
                     <input type="hidden" name="remember" defaultValue="true"/>
-                    <div className="rounded-md shadow-sm -space-y-px">
+                    <div className="rounded-md shadow-xs -space-y-px">
                         <div>
                             <input
                                 name="username"
                                 type="text"
                                 autoComplete="username"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-hidden focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Username"
                                 value={username}
                                 onChange={e => setUsername(e.target.value)}
@@ -98,7 +98,7 @@ export default function Auth() {
                                 type="password"
                                 autoComplete="current-password"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-hidden focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Password"
                                 value={password}
                                 onChange={e => setPassword(e.target.value)}
@@ -119,7 +119,7 @@ export default function Auth() {
                     <div>
                         <button
                             type="submit"
-                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                             <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                               <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
--- components/Layout.js
+++ components/Layout.js
@@ -12,20 +12,20 @@ export default function Layout({children, title = "baseball management system"})
             <header>
                 {/* see: https://tailwindcomponents.com/component/simple-navbar */}
                 <nav
-                    className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-indigo-100 shadow sm:items-baseline w-full">
+                    className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-indigo-100 shadow-sm sm:items-baseline w-full">
                     {/*<div className="flex items-center pl-8 h-14">*/}
                     <div className={"mb-2 sm:mb-0"}>
                         <div className="flex space-x-4">
-                            <span className="text-indigo-500 font-extrabold px-3 py-2 rounded">BMS</span>
-                            <Link href="/" className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded">
+                            <span className="text-indigo-500 font-extrabold px-3 py-2 rounded-sm">BMS</span>
+                            <Link href="/" className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-sm">
                                 Home
                             </Link>
                             {/* TODO: Now working */}
                             <span
-                                className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded">Team (Now working)</span>
+                                className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-sm">Team (Now working)</span>
                             <Link
                                 href="/about"
-                                className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded">
+                                className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-sm">
                                 About
                             </Link>
                         </div>
@@ -33,7 +33,7 @@ export default function Layout({children, title = "baseball management system"})
                     <div>
                         <Link
                             href={"/login"}
-                            className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded">
+                            className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-sm">
                             {/* NOTE: It may be better to split into 2 buttons. */}
                             Log in | Register
                         </Link>
@@ -44,17 +44,17 @@ export default function Layout({children, title = "baseball management system"})
                 {children}
             </main>
             <footer id="footer" className={"w-full h-12 flex justify-center items-center border-t"}>
-                <span className={"text-indigo-700 hover:text-indigo-700 px-5 py-2 rounded"}>
+                <span className={"text-indigo-700 hover:text-indigo-700 px-5 py-2 rounded-sm"}>
                     &copy; 2022 BMS - Baseball Management System
                 </span>
                 <Link
                     href="mailto:bms-dev@googlegroups.com"
-                    className={"text-indigo-300 hover:text-indigo-700 px-5 py-2 rounded"}>
+                    className={"text-indigo-300 hover:text-indigo-700 px-5 py-2 rounded-sm"}>
                     お問い合わせ
                 </Link>
                 {/*
                 <Link href="https://twitter.com/BmsStatus">
-                    <a className={"text-indigo-300 hover:text-indigo-700 px-5 py-2 rounded"}>Twitter</a>
+                    <a className={"text-indigo-300 hover:text-indigo-700 px-5 py-2 rounded-sm"}>Twitter</a>
                 </Link>
                 */}
             </footer>
